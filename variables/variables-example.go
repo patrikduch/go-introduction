6f3ac52be7e8d24f593f7360fe04cffbc259eba5
patrikduch <duchpatrik@icloud.com>
2022-11-16 07:25:17 +0100

    Variables another examples

--- variables/variables-example.go
+++ variables/variables-example.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+// package scope variable declaration
+var name string
+
 func main() {
-	// declaration and initialization numbers array
+
+	// short variable declaration and initialization (block scope)
 	numbers := []int{1, 2}
+
+	// variable assignment
+	name = "Patrik"
+
+	fmt.Printf("Get type of numbers array: %T", numbers)
 	fmt.Println(numbers)
+	fmt.Println("My name is:", name)
 }
